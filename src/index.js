c0b59549ab444c6104173c89065b7a2f6fd426c9
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-04-21 16:12:01 -0400

    fetching done on form

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+
+    //access the input element
+    const inputForm = document.querySelector('form')
+    inputForm.addEventListener('submit', e =>{
+        e.preventDefault()
+        let inputValue = document.querySelector('input#searchByID').value
+
+        // fetch the data
+        fetch(`http://localhost:3000/movies/${inputValue}`)
+        .then(res => res.json())
+        .then (data => {
+            const title = document.querySelector("section#movieDetails h4");
+            const summary = document.querySelector("section#movieDetails p");
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        })
+
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
